f208a26f717e3d426731cd4e18323d7cb7ed1191
Tobias-1991 <62919937+Tobias-1991@users.noreply.github.com>
2022-06-26 17:56:42 -0700

    Added Home Screen, Leaderboard screen, working on game screen mockup.

--- MinesweeperMobile/GameActivity.cs
+++ MinesweeperMobile/GameActivity.cs
@@ -14,6 +14,7 @@ namespace MinesweeperMobile
     [Activity(Label = "GameActivity")]
     public class GameActivity : Activity
     {
+        public Button[,] btnGrid = new Button[10, 10];
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,6 +22,26 @@ namespace MinesweeperMobile
             SetContentView(Resource.Layout.activity_game);
 
             // Create your application here
+            GridView grid = (GridView)FindViewById(Resource.Id.gridView1);
+
+            
+            for (int r=0; r<10; r++)
+            {
+                for (int c = 0; c < 10; c++)
+                {
+                //    btnGrid[r,c] = new Button()
+                //    btnGrid[r, c].Width = buttonSize;
+                //    btnGrid[r, c].Height = buttonSize;
+                //    btnGrid[r, c].MouseDown += Grid_Button_Click;
+                //    panel1.Controls.Add(btnGrid[r, c]);
+                //    btnGrid[r, c].Location = new Point(buttonSize * r, buttonSize * c);
+                //    btnGrid[r, c].Tag = r.ToString() + "|" + c.ToString();
+                //    btnGrid[r, c].BackColor = Color.Cornsilk;
+                //    btnGrid[r, c].BackgroundImageLayout = ImageLayout.Stretch;                
+                }
+            }
+
+            
         }
     }
 }
